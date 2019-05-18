29e0cd6a04372e240ede528722b55e2d99602cc8
Rohit <r4rohit002@gmail.com>
2019-05-18 11:18:21 +0530

    Made role comparable and serializable

--- OMDB/app/src/main/java/com/omdb/rohksin/Mangoose/Redesign/MoshiModels/Role.java
+++ OMDB/app/src/main/java/com/omdb/rohksin/Mangoose/Redesign/MoshiModels/Role.java
@@ -1,8 +1,13 @@
 package com.omdb.rohksin.Mangoose.Redesign.MoshiModels;
 
+import android.support.annotation.NonNull;
+
+import com.omdb.rohksin.Mangoose.Redesign.Utilities.MovieUtils;
+
+import java.io.Serializable;
 import java.util.List;
 
-public class Role {
+public class Role implements Serializable, Comparable<Role>{
 
     public String release_date;
     public boolean adult;
@@ -17,4 +22,14 @@ public class Role {
     public String credit_id;
     public List<Integer> genre_ids;
 
+    @Override
+    public int compareTo(@NonNull Role role) {
+
+        String date1 = MovieUtils.getSortedDate(this.release_date);
+
+        String date2 = MovieUtils.getSortedDate(role.release_date);
+
+        return date2.compareTo(date1);
+
+    }
 }
