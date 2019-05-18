3b25df3d57832bde8f830d7b90e3c0e993e30b9e
Rohit <r4rohit002@gmail.com>
2019-05-18 10:58:31 +0530

    Added Models for Actor

---
+++ OMDB/app/src/main/java/com/omdb/rohksin/Mangoose/Redesign/MoshiModels/Actor.java
@@ -0,0 +1,19 @@
+package com.omdb.rohksin.Mangoose.Redesign.MoshiModels;
+
+public class Actor {
+
+    public String birthday;
+    public String known_for_department;
+    public int id;
+    public String homepage;
+    public String profile_path;
+    public String imdb_id;
+    public String deathday;
+    public String name;
+    public String biography;
+    public boolean adult;
+    public String place_of_birth;
+    public int gender;
+    public MovieCredits movie_credits;
+
+}
---
+++ OMDB/app/src/main/java/com/omdb/rohksin/Mangoose/Redesign/MoshiModels/Role.java
@@ -0,0 +1,20 @@
+package com.omdb.rohksin.Mangoose.Redesign.MoshiModels;
+
+import java.util.List;
+
+public class Role {
+
+    public String release_date;
+    public boolean adult;
+    public String title;
+    public String original_language;
+    public String character;
+    public String original_title;
+    public String poster_path;
+    public int id;
+    public String backdrop_path;
+    public String overview;
+    public String credit_id;
+    public List<Integer> genre_ids;
+
+}
