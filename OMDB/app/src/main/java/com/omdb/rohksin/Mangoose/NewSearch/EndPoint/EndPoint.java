ecd5ab807239452a65cec2357e695d810115f082
Rohit <r4rohit002@gmail.com>
2019-05-19 14:47:13 +0530

    Deleted unused xmls and Java classes

--- OMDB/app/src/main/java/com/omdb/rohksin/Mangoose/NewSearch/EndPoint/EndPoint.java
+++
@@ -1,8 +0,0 @@
-package com.omdb.rohksin.Mangoose.NewSearch.EndPoint;
-
-/**
- * Created by Illuminati on 6/12/2017.
- */
-public interface EndPoint {
-    public String buildEndPoint(Object object);
-}
